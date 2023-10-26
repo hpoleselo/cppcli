0fcc3defd67a626a5e25494b10870c961205996d
Henrique Poleselo <hpoleselo@gmail.com>
2023-10-26 09:24:24 -0300

    Feat: changed variables to camelcase and fixed convertion function.

--- kg2lb/main.cpp
+++ kg2lb/main.cpp
@@ -5,15 +5,17 @@ float kg2pd(float weight_in_kilo) {
     return weight_in_kilo * 2.20462262;
 }
 
-float convertStringToFloat(char* inputFromUser) {
+double convertStringToFloat(char* input_from_user) {
     try {
-        float floatValue = std::stof(inputFromUser);
-        std::cout << "Converted float value: " << floatValue << std::endl;
-        return floatValue;
+        double float_value = std::atof(input_from_user);
+        //std::cout << "Converted float value: " << float_value << std::endl;
+        return float_value;
     } catch (const std::invalid_argument& e) {
         std::cerr << "Invalid input: " << e.what() << std::endl;
+        throw e;
     } catch (const std::out_of_range& e) {
         std::cerr << "Input out of range: " << e.what() << std::endl;
+        throw e;
     }
 } 
 
@@ -32,24 +34,20 @@ int main(int argc, char **argv) {
     }
     else {
         float weight_in_kilo {0};
-        // TODO: Call the conversion function
-        weight_in_kilo = std::stof(argv[0]);
+        weight_in_kilo = convertStringToFloat(argv[1]);
+        //weight_in_kilo = std::stof(argv[0]);
 
         if (weight_in_kilo != 0) {
             // TODO: Add the formatter from precision cout
             float weight_in_pounds {0};
             weight_in_pounds = kg2pd(weight_in_kilo);
-            std::cout << "Computed weight:\n" << weight_in_kilo << " is equivalent to " << weight_in_pounds << " pounds.";
+            std::cout << "Computed weight: " << weight_in_kilo << "kg is equivalent to " << weight_in_pounds << " pounds." << std::endl;
             return 0;
         }
 
         else {
             std::cout << "0 was provided.";
         }
-    }
-    // Access individual arguments
-    for (int i = 1; i < argc; i++) {
-        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
     }
 
     return 0;
