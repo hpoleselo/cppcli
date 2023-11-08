478666f008538ba8f1634091baac554a63116652
Henrique Poleselo <hpoleselo@gmail.com>
2023-11-08 17:02:05 -0300

    Fix: removed std:: for atof.

---
+++ kg2lb/utils.cpp
@@ -0,0 +1,14 @@
+
+#include <stdlib.h> 
+double kg2pd(double weight_in_kilo) {
+    // Converts kilograms to pounds
+    return weight_in_kilo * 2.20462262;
+}
+df
+// TODO: Place this in a Header file?
+double convertStringToDouble(char* input_from_user) {
+    // atof is a C function that returns a double: $ man atof
+    double double_value = atof(input_from_user);
+    //std::cout << "Converted float value: " << double_value << std::endl;
+    return double_value;
+} 
